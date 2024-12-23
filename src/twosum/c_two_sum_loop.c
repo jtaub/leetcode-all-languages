39f5efaa9a4f1400fef63094a24b8dfe5914046b
jtaub <JakeTaubner@ProtonMail.com>
2024-12-23 14:28:26 -0500

    format

--- src/twosum/c_two_sum_loop.c
+++ src/twosum/c_two_sum_loop.c
@@ -1,10 +1,9 @@
-
 #include <stdlib.h>
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
-
     for (int i = 0; i < numsSize; i++) {
         for (int j = i + 1; j < numsSize; j++) {
             if (nums[i] + nums[j] == target) {
@@ -13,7 +12,7 @@ int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
                 result[0] = i;
                 result[1] = j;
                 return result;
-            }            
+            }
         }
     }
 
