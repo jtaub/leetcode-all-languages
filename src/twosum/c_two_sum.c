1caf64c02bc44338a908692a1da9da4f35dbf257
jtaub <JakeTaubner@ProtonMail.com>
2024-12-23 11:30:48 -0500

    support for C

--- src/twosum/c_two_sum.c
+++ src/twosum/c_two_sum.c
@@ -1,6 +1,38 @@
+#include <uthash.h>
+
+struct hashTable {
+    int key;
+    int value;
+    UT_hash_handle hh;
+};
+
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
-    
+int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
+    struct hashTable *map = NULL; 
+
+    for (int i = 0; i < numsSize; i++) {
+        int complement = target - nums[i];
+
+        struct hashTable *found;
+        HASH_FIND_INT(map, &complement, found);
+
+        if (found) {
+            *returnSize = 2;
+            int *result = malloc(2 * sizeof(int));
+            result[0] = found->value;
+            result[1] = i;
+            return result;
+        }
+
+        struct hashTable *entry = malloc(sizeof(struct hashTable));
+        entry->key = nums[i];
+        entry->value = i;
+        HASH_ADD_INT(map, key, entry); 
+    }
+
+    *returnSize = 0;
+    return NULL;
 }
