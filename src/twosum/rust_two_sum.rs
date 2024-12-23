a85f3e619c9fa98ca5d66798747e074c62c9a7a2
jtaub <JakeTaubner@ProtonMail.com>
2024-12-23 15:08:31 -0500

    more functional rust

--- src/twosum/rust_two_sum.rs
+++ src/twosum/rust_two_sum.rs
@@ -1,15 +1,17 @@
 use std::collections::HashMap;
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    let mut map = HashMap::with_capacity(nums.len());
-        
-    for (i, &num) in nums.iter().enumerate() {
-        let complement = target - num;
-        if let Some(&index) = map.get(&complement) {
-            return vec![index as i32, i as i32];
-        }
-        map.insert(num, i);
-    }
+    let mut map: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
 
-    vec![]
+    nums.iter()
+        .enumerate()
+        .find_map(|(i, &num)| {
+            map.get(&(target - num))
+                .map(|match_index| vec![*match_index as i32, i as i32])
+                .or_else(|| {
+                    map.insert(num, i);
+                    None
+                })
+        })
+        .unwrap()
 }
