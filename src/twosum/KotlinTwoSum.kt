d667bfebab3336306bd5c4fea3536a551ddd62f7
jtaub <JakeTaubner@ProtonMail.com>
2024-12-24 10:55:30 -0500

    begin recording

--- src/twosum/KotlinTwoSum.kt
+++ src/twosum/KotlinTwoSum.kt
@@ -1,9 +1,9 @@
 package twosum
 
 fun twoSum(nums: IntArray, target: Int): IntArray {
-    // Map from required value to the index which needs that value
     val numToIndex = HashMap<Int, Int>(nums.size)
     
+    // Assign the result of this expression to a val named "i"
     nums.indices.first { i -> 
         val num = nums[i]
         numToIndex[target - num] = i
