fac8704d508f0088f888c065f09e5ff2d14dc78b
jtaub <JakeTaubner@ProtonMail.com>
2024-12-26 22:34:36 -0500

    racket solution

---
+++ src/twosum/racket_two_sum.rkt
@@ -0,0 +1,10 @@
+(define/contract (two-sum nums target)
+  (-> (listof exact-integer?) exact-integer? (listof exact-integer?))
+
+  (define (go num-to-index remaining_nums i)
+    (define num (car remaining_nums))
+    (match (hash-ref num-to-index num #f)
+      [#f (go (hash-set num-to-index (- target num) i) (cdr remaining_nums) (+ i 1))]
+      [j (list j i)]))
+
+  (go (hash) nums 0))
