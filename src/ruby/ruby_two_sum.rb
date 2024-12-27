53eb60a2c8748446f4aef016f8d1a8703bccb2c8
jtaub <JakeTaubner@ProtonMail.com>
2024-12-26 21:47:03 -0500

    rename ruby

--- src/ruby/ruby_two_sum.rb
+++ src/ruby/ruby_two_sum.rb
@@ -2,12 +2,12 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-  num_to_target = {}
+  num_to_index = {}
   
   nums.each_with_index do |num, i|
-    j = num_to_target[num]
+    j = num_to_index[num]
     return [j, i] if j
-    num_to_target[target - num] = i
+    num_to_index[target - num] = i
   end
   
   raise ArgumentError, "No solution"
