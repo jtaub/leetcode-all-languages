f7b49cfd1776c89f934909cad2f5ed9f5e09f4d2
jtaub <JakeTaubner@ProtonMail.com>
2024-12-27 17:42:36 -0500

    php

---
+++ src/twosum/PhpTwoSum.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace twosum;
+class PhpTwoSum {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum(array $nums, int $target) {
+
+        $numToIndex = [];
+        foreach ($nums as $i => $num) {
+            $j = $numToIndex[$num];
+            
+            if ($j !== null) {
+                return [$j, $i];
+            }
+            
+            $numToIndex[$target - $num] = $i;
+        }
+        
+        throw new \BadFunctionCallException('No solution');
+    }
+}
