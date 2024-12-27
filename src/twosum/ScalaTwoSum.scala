df1cb249d5e8cf71d9bcfe6e95a050b50b7099c6
jtaub <JakeTaubner@ProtonMail.com>
2024-12-26 22:35:12 -0500

    small scala change

--- src/twosum/ScalaTwoSum.scala
+++ src/twosum/ScalaTwoSum.scala
@@ -6,9 +6,8 @@ object ScalaTwoSum:
   def twoSum(nums: Array[Int], target: Int): Array[Int] =
     @tailrec
     def go(numToIndex: Map[Int, Int], i: Int): Array[Int] =
-      val num = nums(i)
-      numToIndex.get(num) match 
+      numToIndex.get(nums(i)) match 
         case Some(j) => Array(j, i)
-        case None => go(numToIndex + (target - num -> i), i + 1)
+        case None => go(numToIndex + (target - nums(i) -> i), i + 1)
   
     go(Map(), 0)
