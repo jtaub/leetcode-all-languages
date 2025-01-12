da40bad96ff3460714fe79879121e2e292137105
jtaub <JakeTaubner@ProtonMail.com>
2025-01-12 12:05:11 -0500

    some small changes

--- Sources/SwiftLeetCode/SwiftContainsDuplicate.swift
+++ Sources/SwiftLeetCode/SwiftContainsDuplicate.swift
@@ -1,6 +1,6 @@
 class SwiftContainsDuplicate {
   func containsDuplicate(_ nums: [Int]) -> Bool {
-    var seen = Set<Int>()
+    var seen = Set<Int>(minimumCapacity: nums.capacity)
     
     return !nums.allSatisfy { num in
       seen.insert(num).inserted
--- src/containsduplicate/rust_contains_duplicate.rs
+++ src/containsduplicate/rust_contains_duplicate.rs
@@ -2,3 +2,8 @@ pub fn contains_duplicate(nums: Vec<i32>) -> bool {
     let mut seen = std::collections::HashSet::with_capacity(nums.len());
     !nums.iter().all(|num| seen.insert(*num))
 }
+
+pub fn contains_duplicate_sorting(mut nums: Vec<i32>) -> bool {
+    nums.sort_unstable();
+    nums.windows(2).any(|window| window[0] == window[1])
+}
