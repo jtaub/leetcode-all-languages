492da7272f758226b69b325df95723abcf70d0bd
jtaub <JakeTaubner@ProtonMail.com>
2025-01-14 08:19:15 -0500

    apply a consistent naming convention

---
+++ performance_results/valid_anagram_results.py
@@ -0,0 +1,26 @@
+from bar_chart_builder import plot_sorted_data_as_png
+
+if __name__ == "__main__":
+    plot_sorted_data_as_png(
+        problem_name="Contains Duplicate",
+        data={
+            # "C": {"time": 35},
+            # "C#": {"time": 13},
+            # "C++": {"time": 17},
+            # "Dart": {"time": 11},
+            # "Elixir": {"time": 70, "image": "Elixir.png"},
+            # "Erlang": {"time": 79, "image": "Erlang.png"},
+            # "Go": {"time": 9},
+            # "Java": {"time": 13},
+            # "JavaScript": {"time": 8, "image": "JavaScript.png"},
+            # "Kotlin": {"time": 13},
+            # "PHP": {"time": 4},
+            # "Python": {"time": 3, "image": "Python.png"},
+            # "Racket": {"time": 57},
+            # "Ruby": {"time": 9},
+            # "Rust": {"time": 0},
+            "Scala": {"time": 34, "image": "Scala.png"},
+            # "Swift": {"time": 4, "image": "Swift.png"},
+            # "TypeScript": {"time": 5},
+        },
+    )
--- src/validanagram/ScalaValidAnagram.scala
+++ src/validanagram/ScalaValidAnagram.scala
@@ -11,36 +11,36 @@ def isAnagramGroupMapReduce(s: String, t: String): Boolean =
   
 def isAnagramOneMap(s: String, t: String): Boolean =
   @tailrec
-  def go(map: Map[Char, Int], i: Int): Boolean =
+  def go(counts: Map[Char, Int], i: Int): Boolean =
     // If we reach the end of the string, then check that all values are 0
-    if i >= t.length then map.values.forall(_ == 0)
-    else map.get(t(i)) match
+    if i >= t.length then counts.values.forall(_ == 0)
+    else counts.get(t(i)) match
       case None => false // Char in t is not in s
       case Some(0) => false // Too many in t
-      case Some(n) => go(map + (t(i) -> (n - 1)), i + 1)
+      case Some(n) => go(counts + (t(i) -> (n - 1)), i + 1)
       
   // Only count the chars for one, then move on to the other
   go(s.groupMapReduce(identity)(_ => 1)(_ + _), 0)
 
 def isAnagramMutableMap(s: String, t: String): Boolean =
-  val count = mutable.Map[Char, Int]()
+  val counts = mutable.Map[Char, Int]()
   
   for (c <- s)
-    count(c) = count.getOrElse(c, 0) + 1
+    counts(c) = counts.getOrElse(c, 0) + 1
     
   for (c <- t)
-    count(c) = count.getOrElse(c, 0) - 1
+    counts(c) = counts.getOrElse(c, 0) - 1
     
-  count.values.forall(_ == 0)
+  counts.values.forall(_ == 0)
 
 def isAnagramArray(s: String, t: String): Boolean =
-  val result = Array.fill(26)(0)
+  val counts = Array.fill(26)(0)
   
   for (c <- s) 
-    result(c - 'a') += 1
+    counts(c - 'a') += 1
     
   for (c <- t)
-    result(c - 'a') -= 1
+    counts(c - 'a') -= 1
     
-  result.forall(_ == 0)
+  counts.forall(_ == 0)
   
