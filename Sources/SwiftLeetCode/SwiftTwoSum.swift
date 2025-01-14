19b5e2520843250aab59722f70a00fe57d3dc082
jtaub <JakeTaubner@ProtonMail.com>
2025-01-14 08:45:50 -0500

    remove extra white space

--- Sources/SwiftLeetCode/SwiftTwoSum.swift
+++ Sources/SwiftLeetCode/SwiftTwoSum.swift
@@ -4,7 +4,6 @@ class TwoSum {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var numToIndex = [Int: Int]()
         
-        
         for (i, num) in nums.enumerated() {
             if let j = numToIndex[num] {
                 return [j, i]
