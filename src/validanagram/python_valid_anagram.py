05d71bf62e82ee82a7cd3465f359d8e888603507
jtaub <JakeTaubner@ProtonMail.com>
2025-01-15 07:48:06 -0500

    counter

--- src/validanagram/python_valid_anagram.py
+++ src/validanagram/python_valid_anagram.py
@@ -1,11 +1,18 @@
+from collections import Counter
+
+
+def isAnagramCounter(s: str, t: str) -> bool:
+    return Counter(s) == Counter(t)
+
+
 def isAnagram(s: str, t: str) -> bool:
-  counts = 26 * [0]
-  a = ord('a')
-  
-  for c in s:
-    counts[ord(c) - a] += 1
-    
-  for c in t:
-    counts[ord(c) - a] -= 1
-    
-  return not any(counts)
+    counts = 26 * [0]
+    a = ord("a")
+
+    for c in s:
+        counts[ord(c) - a] += 1
+
+    for c in t:
+        counts[ord(c) - a] -= 1
+
+    return not any(counts)
