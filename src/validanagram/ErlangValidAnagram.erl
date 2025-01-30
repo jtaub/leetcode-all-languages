5c5e371df03a6174087e978ba3e60f3b7027ef78
jtaub <JakeTaubner@ProtonMail.com>
2025-01-29 21:24:28 -0500

    whitespace

--- src/validanagram/ErlangValidAnagram.erl
+++ src/validanagram/ErlangValidAnagram.erl
@@ -3,10 +3,10 @@ is_anagram(S, T) ->
     Count1 = count_characters(S),
     Count2 = count_characters(T),
     Count1 =:= Count2.
-  
+
 count_characters(Str) ->
     lists:foldl(
-        fun(Char, Acc) -> maps:update_with(Char, fun(X) -> X + 1 end, 1, Acc) end, 
-        #{}, 
+        fun(Char, Acc) -> maps:update_with(Char, fun(X) -> X + 1 end, 1, Acc) end,
+        #{},
         unicode:characters_to_list(Str)
     ).
